14eadfd8b16b7a163e98d215d22ba12c92a49174
Rajeshwari1330 <yallalraj1307@gmail.com>
2020-05-14 20:13:42 +0530

    changed contents

--- f1.java
+++ f1.java
@@ -2,6 +2,6 @@ class f1
 {
 	public static void main(String[] args)
 	{
-		System.out.println(" I am creating a file in local repository " );
+		System.out.println(" changing a statement " );
 	}
 }
